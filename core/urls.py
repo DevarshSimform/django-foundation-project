934bc8ded8763949e0855a8a50370ef0a92a4d31
Devarsh <devarsh.chhatrala@simformsolutions.com>
2025-03-07 14:49:43 +0530

    Created views and build tweet_list template

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home_page'),
+    path('', views.tweet_list, name='tweet_list'),
+    path('create/', views.tweet_create, name='tweet_create'),
+    path('<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
+    path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
     
 ] 
--- core/views.py
+++ core/views.py
@@ -1,4 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Tweet
+from .forms import TweetForm
 
 def home_page(req):
     return render(req, 'core/base.html') 
+
+
+def tweet_list(req):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(req, 'core/tweet_list.html', {'tweets': tweets})
+
+
+def tweet_create(req):
+    if req.method == 'POST':
+        form = TweetForm(req.POST, req.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = req.user
+            tweet.save()
+            return redirect('tweet_list')
+        pass
+    else:
+        form = TweetForm()
+    return render(req, 'core/tweet_form.html', {'form': form})
+
+
+def tweet_edit(req, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user = req.user)
+    if req.method == 'POST':
+        form = TweetForm(req.POST, req.FILES, instance=tweet)
+        if form.is_valid:
+            tweet = form.save(commit=False)
+            tweet.user = req.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm(instance=tweet)
+    return render(req, 'core/tweet_form.html', {'form': form})
+
+
+def tweet_delete(req, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=req.user)
+    if req.method == 'POST':
+        tweet.delete()
+        return redirect('tweet_list')
+    return render(req, 'core/tweet_confirm_delete.html', {'tweet': tweet})
