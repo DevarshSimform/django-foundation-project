53e87c7bf0c3767dac7a85204b2d046e73976cb5
Devarsh <devarsh.chhatrala@simformsolutions.com>
2025-03-10 18:16:21 +0530

    connected postgres and added new data

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'twitter_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tweets',
+        'USER': 'demouser',
+        'PASSWORD': 'demopassword',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
